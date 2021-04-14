ceb086fce4bc2b36e1ddb32ce5c30f008e8eed4c
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2021-04-14 19:53:12 +0100

    Building the logical of the game

---
+++ assets/js/perguntas.js
@@ -0,0 +1,17 @@
+let pergunta = [
+	{
+		pergunta: 'QUEM CRIOU ESTE JOGO?',
+		respostas:['Rigoberto Caionda','Mourinho Gabriel','Saludson Correia','Rodrigues Soares'],
+		respostaCerta: 'Rigoberto Caionda'
+	},
+	{
+		pergunta: 'Qual e a minha cor favorita?',
+		respostas:['azul','Branco','Preto','Castanho'],
+		respostaCerta: 'Branco'
+	},
+	{
+		pergunta: 'Qual e a meu pais favorito?',
+		respostas:['Russia','Angola','Irlanda','Alemanha'],
+		respostaCerta: 'Alemanha'
+	}
+];
--- assets/js/scriptNewGamePreparation.js
+++ assets/js/scriptNewGamePreparation.js
@@ -6,11 +6,25 @@ let countNumber = document.querySelector('.second-group--countdown i');
 let container1 = document.querySelector('.container');
 let container2 = document.querySelector('.container2');
 let container3 = document.querySelector('.container3');
+let question = document.querySelector('#question');
 let close = document.querySelector('#close');
+let answerA = document.querySelector('#answerA');
+let answerB = document.querySelector('#answerB');
+let answerC = document.querySelector('#answerC');
+let answerD = document.querySelector('#answerD');
+let option = document.querySelectorAll('.option');
+let resp = document.querySelectorAll('.answer');
+let letra = document.querySelectorAll('.letter');
 let time = document.querySelector('.time');
 let lostImage = document.querySelector('.lost-img img');
 let rescueData = document.querySelector('#rescue-data');
 let timer2;
+//Controles essenciais
+let megasGanhos = 1;
+let premio = [1,3,5,10,20,30,50,100,200,300,400,500,1000];//Os mb que ele pode ganhar
+let numeroDaPergunta = 1;
+let fase = 1;
+//controles essencias fechamento
 let mainCount = 30;
 let timer = setInterval(()=>{
 	count--;
@@ -34,6 +48,41 @@ let timer = setInterval(()=>{
 		},1000);
 	}
 },1000);
+let randomQuestion = Math.round(Math.random() * (pergunta.length - 1));
+question.innerHTML = pergunta[randomQuestion].pergunta.toUpperCase();
+answerA.innerHTML = pergunta[randomQuestion].respostas[0].toUpperCase();
+answerB.innerHTML = pergunta[randomQuestion].respostas[1].toUpperCase();
+answerC.innerHTML = pergunta[randomQuestion].respostas[2].toUpperCase();
+answerD.innerHTML = pergunta[randomQuestion].respostas[3].toUpperCase();
+function validateAnswer(){
+
+}
+option.forEach((item)=>{
+	item.addEventListener('click',(e)=>{
+		e.target.closest('.option').querySelector('.answer').classList.add('pintaAnswer');
+		e.target.closest('.option').querySelector('.letter').classList.add('pintaLetter');
+		setTimeout(()=>{
+				if(pergunta[randomQuestion].respostaCerta.toUpperCase() == e.target.closest('.option').querySelector('.answer').innerHTML.toUpperCase()){
+					e.target.closest('.option').querySelector('.answer').classList.remove('pintaAnswer');
+					e.target.closest('.option').querySelector('.answer').classList.add('certoAnswer');
+					e.target.closest('.option').querySelector('.letter').classList.remove('pintaLetter');
+					e.target.closest('.option').querySelector('.letter').classList.add('certoLetter');
+				}else{
+					e.target.closest('.option').querySelector('.answer').classList.remove('pintaAnswer');
+					e.target.closest('.option').querySelector('.answer').classList.add('errouAnswer');
+					e.target.closest('.option').querySelector('.letter').classList.remove('pintaLetter');
+					e.target.closest('.option').querySelector('.letter').classList.add('errouLetter');
+					for(let i = 0; i < resp.length; i++){
+						if(pergunta[randomQuestion].respostaCerta.toUpperCase() == resp[i].innerHTML.toUpperCase()){
+							resp[i].classList.add('certoAnswer');
+							letra[i].classList.add('certoLetter');
+						}
+					}
+				}
+		},2000);
+		//Verificar se existe o idList, tem como pegar o id ou a class de um elemento?
+	});
+});
 close.addEventListener('click',()=>{
 	window.location = 'newGame.html';
 });
