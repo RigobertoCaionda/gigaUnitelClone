24bb115e2367c971b416f37af8479742dbe46877
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2021-07-07 23:02:38 +0100

    Adding more questions to the game

--- assets/js/perguntas.js
+++ assets/js/perguntas.js
@@ -5,13 +5,28 @@ let pergunta = [
 		respostaCerta: 'Rigoberto Caionda'
 	},
 	{
-		pergunta: 'Qual e a minha cor favorita?',
-		respostas:['azul','Branco','Preto','Castanho'],
-		respostaCerta: 'Branco'
+		pergunta: 'Quanto é 1 * 1?',
+		respostas:['2','4','6','1'],
+		respostaCerta: '1'
 	},
 	{
-		pergunta: 'Qual e o meu pais favorito?',
-		respostas:['Russia','Angola','Irlanda','Alemanha'],
-		respostaCerta: 'Alemanha'
+		pergunta: 'Em que continente se encontra Angola?',
+		respostas:['América','Ásia','África','Europa'],
+		respostaCerta: 'África'
+	},
+	{
+		pergunta: 'Qual desses já foi melhor jogador do mundo?',
+		respostas:['Lionel Messi','Mário Balotelli','Phil Phoden','Sérgio Aguero'],
+		respostaCerta: 'Lionel Messi'
+	},
+	{
+		pergunta: 'Qual desses não é uma linguagem de programação?',
+		respostas:['Java','Html','Python','C#'],
+		respostaCerta: 'Html'
+	},
+	{
+		pergunta: 'Qual desses não é um cantor?',
+		respostas:['Cristiano Ronaldo','Adele','Sia','Justin Bieber'],
+		respostaCerta: 'Cristiano Ronaldo'
 	}
 ];
